cc25b287d364a6f273a010d33354e0dddf5274c6
Cesaryuan <35998162+cesaryuan@users.noreply.github.com>
2022-11-15 16:03:32 +0800

    Update JSFinder.js
    remove redundant dependency

--- DevTools_JSFinder/JSFinder.js
+++ DevTools_JSFinder/JSFinder.js
@@ -5,7 +5,6 @@
 // @description  Extract interfaces from html and javascript files.
 // @author       Threezh1
 // @match        *://*/*
-// @require      https://greasyfork.org/scripts/12447-mootools-for-greasemonkey/code/MooTools%20for%20Greasemonkey.js?version=74469
 // @grant        none
 // ==/UserScript==
 
@@ -13,7 +12,7 @@
     'use strict';
     console.log("JSFinder by Threezh1");
     let urls = []; let js_content=""; let result_raw = []; let domains = [];
-    $$('*').forEach(element => {
+    [...document.querySelectorAll('*')].forEach(element => {
         urls.push(element.src);urls.push(element.href);urls.push(element.url);
         if (element.tagName == "SCRIPT") { js_content += element.text }
     }); urls = new Set(urls);
